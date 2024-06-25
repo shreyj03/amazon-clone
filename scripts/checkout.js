e5fb5b15e079e73158e1b08fc4daf870341178d3
shreyj03 <jainshrey2004@gmail.com>
2024-06-25 20:03:17 +0530

    made delete functional

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
   }); 
 
 cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">
     Delivery date: Wednesday, June 15
   </div>
@@ -40,7 +40,7 @@ cartSummaryHTML += `
         <span class="update-quantity-link link-primary">
           Update
         </span>
-        <span class="delete-quantity-link link-primary">
+        <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
           Delete
         </span>
       </div>
@@ -96,4 +96,13 @@ cartSummaryHTML += `
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
-console.log(cartSummaryHTML);
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId); 
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      container.remove();
+    });
+  });
