073b25ba8c64fd62ee7fd31036383d439fb31879
shreyj03 <jainshrey2004@gmail.com>
2024-06-27 12:13:50 +0530

    converted products into class

--- data/products.js
+++ data/products.js
@@ -1,6 +1,8 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+
 export function getProduct(productId) {
   let matchingProduct;
-
   products.forEach((product) => {
     if (product.id === productId) {
       matchingProduct = product;
@@ -8,6 +10,31 @@ export function getProduct(productId) {
   });
   return matchingProduct;
 }
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id
+    this.image = productDetails.image
+    this.name = productDetails.name
+    this.rating = productDetails.rating
+    this.priceCents = productDetails.priceCents 
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`
+  }
+}
+
 
 export const products = [  
   {
@@ -668,4 +695,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,6 @@
 import {renderOrderSummary} from "./orderSummary.js";
 import {renderPaymentSummary} from "./paymentSummary.js";
-import '../data/cart-oop.js';
 
 renderOrderSummary();
 
-renderPaymentSummary();
+renderPaymentSummary(); 
